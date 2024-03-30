8965c67dafb642be5ce60923d2bd750cea9031b5
ChernyshovVadym <vad14@i.ua>
2024-03-30 12:22:57 +0200

    add

--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,10 +1,30 @@
-// import { getPhotos } from 'apiService/photos';
+import { getPhotos } from 'apiService/photos';
 import { Text } from 'components';
 import { Form } from 'components';
+import { useEffect } from 'react';
 import { useState } from 'react';
 
 export const Photos = () => {
   const [query, setQuery] = useState('');
+  const [page, setPege] = useState(1);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    if (!query) return;
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const data = await getPhotos(query, page);
+        console.log(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [page, query]);
 
   const onHandleSubmit = value => {
     setQuery(value);
